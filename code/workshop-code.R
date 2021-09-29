3283ad2a033a1cc543509864d923a7ed4168270a
icrichmond <icrichmond@mun.ca>
2021-09-29 13:30:50 -0400

    Code for the workshop

---
+++ code/workshop-code.R
@@ -0,0 +1,8 @@
+# This is all the code chunks used throughout the workshop, feel free to copy and paste them from here if it makes it easier to follow along 
+# Author: Isabella Richmond 
+
+# Loading packages 
+p <- c("tidyverse", "data.table", "anytime")
+lapply(p, library, character.only = T)
+
+# 
